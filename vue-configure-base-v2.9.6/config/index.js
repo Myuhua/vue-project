1fe42b4a89b75ac8302f38c1c8bd4eb0081d2240
yuhua <hua.yu@sdgharm.com>
2020-05-11 17:28:25 +0800

    基于vue-cli-v2.9.6根据实际开发对目录结构改造配置

--- vue-configure-base-v2.9.6/config/index.js
+++ vue-configure-base-v2.9.6/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     // Various Dev Server settings
     // dev-server的ip、端口号，可以自行更改
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8088, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8092, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     // 是否自定代开浏览器
     autoOpenBrowser: false,
     // 下面两个都是浏览器展示错误的方式
